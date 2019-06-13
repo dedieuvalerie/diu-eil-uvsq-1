1bce0267529e86018cf29876d385bf46dca58d04
dedieuvalerie <valerie.dedieu@gmail.com>
2019-06-13 16:27:13 +0200

    Modification statut porte

--- MouvementFoule/Environnement.py
+++ MouvementFoule/Environnement.py
@@ -4,7 +4,7 @@ import Grille
 
 class Voyageur :
     """ cree et determine un voyageur ; attributs : une couleur (tuple de 3 entiers) et une liste trajet contenant les
-    coordonnees des differentes etapes pour arriver a la porte finale (tuples de deux entiers) ; cette liste est mise a jour au fur et a mesure
+    differentes etapes pour arriver a la porte finale (tuple de portes) ; cette liste est mise a jour au fur et a mesure
     que les etapes sont franchies ; lorsqu'elle est vide, le voyageur a atteint son but. Ce voyageur appartient a une grille
     (dictionnaire) """
     
@@ -15,14 +15,15 @@ class Voyageur :
         
     def autourVoyageur(self,position):
         """determine le contenu des cases autour du voyageur (soit un autre voyageur, soit un obstacle, soit une porte) ;
-        en entree : les coordonnees du voyageur (tuple d'entiers) ; en sortie : la listes des cases accessibles (tuples d'entiers) """
+        en entree : les coordonnees du voyageur (tuple d'entiers) ; en sortie : la liste des coordonnees des cases accessibles (tuples d'entiers)
+        et avec leur contenu (porte ou vide) """
         casespossibles=[]
         x_voy=position[0]
         y_voy=position[1]
-        for case in [(x_voy-1, y_voy-1), (x_voy-1, y_voy), (x_voy-1, y_voy+1), (x_voy, y_voy-1), (x_voy, y_voy+1), (x_voy+1, y_voy-1),(x_voy+1, y_voy), (x_voy+1, y_voy+1)]:
-            contenu = self.grille.getContenuCase(case)
+        for coord in [(x_voy-1, y_voy-1), (x_voy-1, y_voy), (x_voy-1, y_voy+1), (x_voy, y_voy-1), (x_voy, y_voy+1), (x_voy+1, y_voy-1),(x_voy+1, y_voy), (x_voy+1, y_voy+1)]:
+            contenu = self.grille.getContenuCase(coord)
             if contenu[0]==None and contenu[1]==None: # case accessible
-                casespossibles.append(case)               
+                casespossibles.append((coord, contenu[2]))               
         return casespossibles
             
         
@@ -30,26 +31,27 @@ class Voyageur :
         """ deplacement d'un voyageur. Sortie : nouvelle position du voyageur (tuple d'entiers).
         Le voyageur commence par observer les cases alentour et etablit ainsi une liste de possibilites.
         Si une des cases accessibles est une des etapes de son trajet, il y va. Sinon, on choisit celle le devie le moins possible de son trajet
-        (produit scalaire) """
+        (produit scalaire de deux vecteurs) """
         maposition=self.grille.getPosition(self)
         casespossibles=autourVoyageur(maposition) 
         if casespossibles==[] : return(maposition) # aucune case accessible, le voyageur ne bouge pas
         else :
             maxi = -1
-            for coord in casespossibles:
-                if coord in self.trajet :
-                    self.trajet.pop(coord) # suppression de l'etape
+            for case in casespossibles:           #on parcourt la liste des cases accessibles
+                if case[1] in self.trajet :       # la case est une porte du trajet du voyageur
+                    self.trajet.pop(case[1]) # suppression de l'etape
                     if self.trajet ==[] : grille.deleteVoyageur(self) # le voyageur est arrive ; on le supprime de la grille
-                    return coord
+                    return case[0]
                 else :
-                    vecteur1=(self.trajet[0][0] - maposition[0], self.trajet[0][1] - maposition[1])
+                    coord_etape=self.grille.getPosition(self.trajet[0]) # on recupere les coordonnees de la porte a atteindre                   
+                    vecteur1=(coord_etape[0] - maposition[0], coord_etape[1] - maposition[1])
                     vecteur2=(coord[0] - maposition[0], coord[1] - maposition[1])
-                    cosinus = (vecteur1[0]*vecteur2[0] + vecteur1[1]*vecteur2[1])/(sqrt(vecteur1[0]**2 + vecteur1[1])*sqrt(vecteur2[0]**2 + vecteur2[1]))
+                    cosinus = (vecteur1[0]*vecteur2[0] + vecteur1[1]*vecteur2[1])/(sqrt(vecteur1[0]**2 + vecteur1[1]**2)*sqrt(vecteur2[0]**2 + vecteur2[1]**2))
                     if cosinus >= maxi :
                         maxi = cosinus
                         new_pos=coord
-                    
-        return new_pos
+            if maxi < 0 : return(maposition) # le voyageur est dans une situation ou il devrait reculer. Il ne bouge pas.
+            return new_pos
     
     
 class Obstacle:
