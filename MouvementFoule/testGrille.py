778a9f23ef0bfef460f8ffba59b14bf8f2cca169
dedieuvalerie <valerie.dedieu@gmail.com>
2019-06-14 11:39:06 +0200

    Amelioration boucle

--- MouvementFoule/testGrille.py
+++ MouvementFoule/testGrille.py
@@ -15,8 +15,9 @@ if __name__ == '__main__':
     p2 = environnement.Porte(vert, g)
     p3 = environnement.Porte(vert, g)
     v1 = environnement.Voyageur(rouge, [p0, p1], g)
-    v2 = environnement.Voyageur(rouge, [p0, p1], g)
+    v2 = environnement.Voyageur(rouge, [p0, p1], g,0.5)
     v3 = environnement.Voyageur(rouge, [p2, p0, p3], g)
+    listeVoyageurs = [v1, v2, v3]
     obs1 = environnement.Obstacle(noir, g)
     g.addObstacle([(0, 0), (5, 4), (5, 5), (6, 5), (9, 9), (8, 5), (9, 5), (10, 5), (11, 5)], obs1)
     g.addVoyageur((1, 1), v1)
@@ -29,8 +30,7 @@ if __name__ == '__main__':
     step = 0
     print(step)
     print(g)
-    while (v1 in g.getVoyageurs().values() or v2 in g.getVoyageurs().values() or 
-           v3 in g.getVoyageurs().values()):
+    while list(filter(lambda v : v in g.getVoyageurs().values(),listeVoyageurs)) != []:
         step += 1
         g.deplacements()
         print(step)
